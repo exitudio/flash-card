dd8ff3d092d25ae99f27a543adcd18c9295be7d6
Ekkasit Pingpong <Ekkasit.Pinyoanunatpon@coxautoinc.com>
2019-07-24 10:45:31 -0400

    restructure data server

--- server/index.js
+++ server/index.js
@@ -3,14 +3,15 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var fallback = require("express-history-api-fallback");
 var fs = require("fs");
+var axios = require("axios");
 
 var app = express();
 app.use(bodyParser.json());
 app.get("/api/getwords", function(req, res) {
-  const wordData = fs.readFileSync(`server/${req.query.type}.txt`, "utf8");
+  const wordData = fs.readFileSync(`server/data/${req.query.type}.txt`, "utf8");
   const rawWords = wordData.split("\n");
 
-  const starData = fs.readFileSync("server/stars.json", "utf8");
+  const starData = fs.readFileSync("server/data/stars.json", "utf8");
   const starObject = JSON.parse(starData);
 
   const words = [];
@@ -27,14 +28,40 @@ app.get("/api/getwords", function(req, res) {
 });
 
 app.post("/api/poststar", function(req, res) {
-  const data = fs.readFileSync("server/stars.json", "utf8");
+  const data = fs.readFileSync("server/data/stars.json", "utf8");
   const starData = JSON.parse(data);
   starData[req.body.word] = req.body.star;
-  fs.writeFileSync("server/stars.json", JSON.stringify(starData));
+  fs.writeFileSync("server/data/stars.json", JSON.stringify(starData));
   return res.status(200).json({ status: "success" });
 });
 
+app.get("/api/get_data_list", function(req, res) {
+  fs.readdir("server/data", function(err, files) {
+    if (err) {
+      return console.log("Unable to scan directory: " + err);
+    }
+    files = files
+      .filter(function(file) {
+        return file.split(".").splice(-1)[0] === "txt";
+      })
+      .map(function(file) {
+        return file;
+      });
+    return res.status(200).json({ status: "success", payload: files });
+  });
+});
+
+app.get("/api/override_from_server", async function(req, res) {
+  const baseUrl = "http://localhost:4001";
+  const listData = await axios.get(`${baseUrl}/api/get_data_list`);
+  console.log("data:", listData.data.payload);
+  const textData = await axios.get(`${baseUrl}/${listData.data.payload[0]}`);
+  console.log("textData:", textData.data);
+  fs.writeFileSync(`server/data/${listData.data.payload[0]}`, textData.data);
+});
+
 app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "../server/data")));
 app.use(fallback("../build/index.html", { root: __dirname }));
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
